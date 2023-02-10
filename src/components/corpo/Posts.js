b7de1fa2a58f85183979b46c20f528f0673a78eb
Viniccius Wirz <vinimovel23@gmail.com>
2023-02-09 21:50:43 -0300

    Apply name changing function, change semantics

--- src/components/corpo/Posts.js
+++ src/components/corpo/Posts.js
@@ -1,23 +1,22 @@
 export default function Posts() {
-    const postInfo =[
-        {id: 1, op: "meowed", post:"gato-telefone", likeHighlight:"respondeai", likes:"101.523"},
-        {id: 2, op: "barked", post:"dog", likeHighlight:"adorable_animals", likes:"99.159"},
+    const postInfo = [
+        { id: 1, op: "meowed", postContent: "gato-telefone", postType: "img", likeHighlight: "respondeai", likes: "101.523" },
+        { id: 2, op: "barked", postContent: "dog", postType: "img", likeHighlight: "adorable_animals", likes: "99.159" },
+        { id: 3, op: "wawawicomics", postContent: "video", postType: "video", likeHighlight: "chibirdart", likes: "55.521" },
     ]
     return (
         <div className="posts">
-            {postInfo.map((e) => <Post key={e.id} user={e.op} postvisual={e.post} lastLike={e.likeHighlight} likeAmount={e.likes}/>)}
+            {postInfo.map((post) => <Post key={post.id} postType={post.postType} user={post.op} postContent={post.postContent} likeHighlight={post.likeHighlight} likes={post.likes} />)}
         </div>
     )
 }
 function Post(props) {
     return (
         <div className="post">
-            <PostTopo usuario={props.user}/>
+            <PostTopo usuario={props.user} />
 
-            <div className="conteudo">
-                <img src={"./assets/img/" + props.postvisual + ".svg"} alt={props.postvisual} />
-            </div>
-            
+            <Content postContent={props.postContent} postType={props.postType} />
+
             <div className="fundo">
                 <div className="acoes">
                     <div>
@@ -30,34 +29,53 @@ function Post(props) {
                     </div>
                 </div>
 
-                <PostLike lastLikeHighlight={props.lastLike} amountOfLikes={props.likeAmount}/>
+                <PostLike likeHighlight={props.likeHighlight} likes={props.likes} />
 
             </div>
         </div>
     )
 }
 
-function PostTopo(props){
-    return(
+function Content(props) {
+    if (props.postType === "img") {
+        return (
+            <div className="conteudo">
+                <img src={"./assets/img/" + props.postContent + ".svg"} alt={props.postContent} />
+            </div>
+        )
+    }
+    if (props.postType === "video") {
+        return (
+            <video autoPlay="autoplay" loop="" muted>
+                <source src={"./assets/img/" + props.postContent + ".mp4"} />
+                <source src={"./assets/img/" + props.postContent + ".ogg"} />
+                Seu navegador não suporta vídeos
+            </video>
+        )
+    }
+}
+
+function PostTopo(props) {
+    return (
         <div className="topo">
-        <div className="usuario">
-            <img src={"./assets/img/" + props.usuario + ".svg"} alt={props.usuario} />
-            {props.usuario}
+            <div className="usuario">
+                <img src={"./assets/img/" + props.usuario + ".svg"} alt={props.usuario} />
+                {props.usuario}
+            </div>
+            <div className="acoes">
+                <ion-icon name="ellipsis-horizontal"></ion-icon>
+            </div>
         </div>
-        <div className="acoes">
-            <ion-icon name="ellipsis-horizontal"></ion-icon>
-        </div>
-    </div>
     )
 }
 
-function PostLike(props){
-    return(
+function PostLike(props) {
+    return (
         <div className="curtidas">
-        <img src={"./assets/img/" + props.lastLikeHighlight + ".svg"} alt={props.lastLikeHighlight} />
-        <div className="texto">
-            Curtido por <strong>{props.lastLikeHighlight}</strong> e <strong>outras {props.amountOfLikes} pessoas</strong>
+            <img src={"./assets/img/" + props.likeHighlight + ".svg"} alt={props.likeHighlight} />
+            <div className="texto">
+                Curtido por <strong>{props.likeHighlight}</strong> e <strong>outras {props.likes} pessoas</strong>
+            </div>
         </div>
-    </div>
     )
 }
--- src/components/corpo/Usuario.js
+++ src/components/corpo/Usuario.js
@@ -1,13 +1,23 @@
-export default function Usuario(){
-    return (
-        <div className="usuario">
-        <img src="./assets/img/catanacomics.svg" alt="imagem de perfil"/>
-        <div className="texto">
-          <span>
-            <strong>catanacomics</strong>
-            <ion-icon name="pencil"></ion-icon>
-          </span>
-        </div>
+import { useState } from "react";
+
+export default function Usuario() {
+  const [userName, setUserName] = useState('catanacomics')
+
+  function changeName() {
+    let newName = prompt('Digite um novo nome de exibição.');
+    let nameVerify = newName ? newName : userName
+    setUserName(nameVerify);
+  }
+
+  return (
+    <div className="usuario">
+      <img src="./assets/img/catanacomics.svg" alt="imagem de perfil" />
+      <div className="texto">
+        <span>
+          <strong>{userName}</strong>
+          <ion-icon name="pencil" onClick={changeName}></ion-icon>
+        </span>
       </div>
-    )
+    </div>
+  )
 }
